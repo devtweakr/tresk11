a199decfea85cfe86d88f1b0cce567d94cba4b89
devtweakr <devtweakr@gmail.com>
2020-01-29 08:47:14 +0100

    more cleanup, imena fajlov urejena

--- js/counter.js
+++ js/counter.js
@@ -10,9 +10,9 @@ let countDown = new Date('Mar 8, 2020 00:00:00').getTime(),
           distance = countDown - now;
 
       document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+      document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+      document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+      document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
       
       //do something later when date is reached
       //if (distance < 0) {
